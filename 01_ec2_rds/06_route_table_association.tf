ff5362d3d39265e5ef32d8d6ee5f01e956f1a59e
Kazuhiko <sato@tent.sakura.ne.jp>
2018-07-25 02:12:47 +0900

    modified route table associations for subnets in ap-northeast-1c/d to enable direct connection to the Internet.

--- 01_ec2_rds/06_route_table_association.tf
+++ 01_ec2_rds/06_route_table_association.tf
@@ -2,3 +2,13 @@ resource "aws_route_table_association" "route_table_association_main" {
   route_table_id = "${aws_route_table.route_table_main.id}"
   subnet_id = "${aws_subnet.subnet_ap_a.id}"
 }
+
+resource "aws_route_table_association" "route_table_association_main" {
+  route_table_id = "${aws_route_table.route_table_main.id}"
+  subnet_id = "${aws_subnet.subnet_ap_c.id}"
+}
+
+resource "aws_route_table_association" "route_table_association_main" {
+  route_table_id = "${aws_route_table.route_table_main.id}"
+  subnet_id = "${aws_subnet.subnet_ap_d.id}"
+}
